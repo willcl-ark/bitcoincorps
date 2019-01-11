311badca6a4d39cf20e46113015e9ae3b8dd139a
willcl-ark <will8clark@gmail.com>
2019-01-11 15:59:36 +0000

    added VersionMessage, Verack and started Addr

--- will/my_crawler.py
+++ will/my_crawler.py
@@ -1,81 +1,87 @@
 import hashlib
-import chainparams
-import time
 import struct
+import time
+import random
 
 from requests import get
 
+import chainparams
 
 LOCAL_WAN_IP = get('https://api.ipify.org').text
+LOCAL_PORT = 8333
 PEER_IP = "77.98.116.8"
 PEER_PORT = 8333
 
+
 # Almost all integers are encoded in little endian. Only IP or port number are encoded big endian.
+# And magic bytes looks like!
 
 
 class Serializable:
 
     @staticmethod
-    def _to_bytes(msg, length=None, byteorder='little'):
-        if isinstance(msg, int):    # or isinstance(msg, bool):
-            if length == None:
+    def _to_bytes(msg, length=0, byteorder='little'):
+        if isinstance(msg, bytes):
+            return msg
+        if isinstance(msg, int):  # or isinstance(msg, bool):
+            if length == 0:
                 length = msg.bit_length()
             return msg.to_bytes(length, byteorder)
         elif isinstance(msg, str):
             return msg.encode(encoding='UTF-8', errors='strict')
-        elif isinstance(msg, bytes):
-            return msg
+        # TODO: add float support?
         else:
-            return print("message of type %s not supported by to_bytes()" % type(msg))
-
-    @staticmethod
-    def pad_msg_end(msg, length):
-        len_diff = length - len(msg)
-        msg += b"\x00" * len_diff
-        return msg
+            return print("message of type %s not supported by _to_bytes()" % type(msg))
 
 
 class Message(Serializable):
 
     def __init__(self, command, payload):
-        self.magic = chainparams.mainParams.StartString.to_bytes(4, byteorder='little')
+        self.magic = struct.pack('>I', chainparams.mainParams.StartString)
         self.command = command
         self.command_bytes = None
-        self.length = len(payload).to_bytes(length=4, byteorder='little')
+        self.length = struct.pack('<I', len(payload))
         self.payload = payload
         self.checksum = None
         self.header = None
 
     def serialize_payload(self):
         if not isinstance(self.payload, bytes):
-            self.payload = Serializable._to_bytes(self.payload)
-            self.length = len(self.payload).to_bytes(length=4, byteorder='little')
-            self.checksum = hashlib.sha256(hashlib.sha256(self.payload).digest()).digest()[:4]
+            self.payload = super()._to_bytes(self.payload)
+        self.length = struct.pack('<I', len(self.payload))
+
+        double_hash = hashlib.sha256(hashlib.sha256(self.payload).digest()).digest()
+        self.checksum = struct.pack('<4s', double_hash[:4])
 
     def generate_header(self):
         self.serialize_payload()
-        self.command_bytes = Serializable.pad_msg_end(Serializable._to_bytes(self.command), 12)
-        self.length = Serializable.pad_msg_end(Serializable._to_bytes(self.length), 4)
-        self.checksum = hashlib.sha256(hashlib.sha256(self.payload).digest()).digest()[:4]
 
-        self.header = b''.join([self.magic, self.command_bytes, self.length, self.checksum])
-        return self.header
+        # serialize and pack command message
+        b = super()._to_bytes(self.command)
+        self.command_bytes = struct.pack('<12s', b)
+
+        # Create the whole header
+        self.header = b"".join([self.magic, self.command_bytes, self.length, self.checksum])
 
     def to_bytes(self):
         self.generate_header()
-        msg = b''.join([self.header, self.payload])
+        msg = b"".join([self.header, self.payload])
         return msg
 
     @staticmethod
     def to_var_int(x):
-        if x < 0xfd:
-            return Serializable._to_bytes(x, length=1)
-        elif x <= 0x10000:
-            return b"\xfd" + Serializable._to_bytes(x, length=3)
-        elif x <= 0x100000000:
-            return b"\xfe" + Serializable._to_bytes(x, length=5)
-        elif x <= 0x10000000000000000:
-            return b"\xff" + Serializable._to_bytes(x, length=9)
+        if x < 0xFD:
+            # pack as uint8_t
+            return struct.pack('<B', x)
+        elif x <= 0xFFFF:
+            # pack as uint16_t
+            return b"\xFD" + struct.pack('<H', x)
+        elif x <= 0xFFFFFFFF:
+            # pack as uint32_t
+            return b"\xFE" + struct.pack('<I', x)
+        elif x <= 0xFFFFFFFFFFFFFFFF:
+            # pack as uint64_t
+            return b"\xFF" + struct.pack('<Q', x)
         else:
             raise RuntimeError("integer too large for type<var_int>")
 
@@ -83,59 +89,96 @@ class Message(Serializable):
     def to_var_str(x):
         s = Serializable._to_bytes(x)
         l = len(s)
-        return Message.to_var_int(l) + s
+        ss = struct.pack('<%ss' % l, s)
+        return Message.to_var_int(l) + ss
 
-    @staticmethod
-    def generate_network_address(ip, port):
-        t = struct.pack(b"<q", int(time.time()))
-        print(t)
-        s = Serializable._to_bytes('services')
-        print(s)
-        p = Serializable._to_bytes(port, byteorder='big')
-        print(p)
+
+class NetworkAddress(Serializable):
+
+    def __init__(self, ip, services=1, port=8333):
+        self.time = struct.pack(b"<I", int(time.time()))
+        self.services = struct.pack('<Q', services)
 
         if ':' in ip:
-            addr = bytes(map(int, ip.split(':')))
+            self.ip = bytes(map(int, ip.split(':')))
         else:
-            addr = (b"\x00" * 10) + (b"\xFF" * 2)
-            addr_bytes = bytes(map(int, ip.split('.')))
-            addr += addr_bytes
+            a = (b"\x00" * 10) + (b"\xFF" * 2)
+            a_bytes = bytes(map(int, ip.split('.')))
+            a += a_bytes
+            self.ip = struct.pack('>16s', a)
 
-        print(addr)
+        self.port = struct.pack('>H', port)
+        self.address = b"".join([self.time, self.services, self.ip, self.port])
+        self.addr_NT = b"".join([self.services, self.ip, self.port])
 
-        return b"".join([t, s, addr, p])
-
-
-
-
-
-
+    def regenerate(self, _services, _ip, _port):
+        self.__init__(services=_services, ip=_ip, port=_port)
 
 
-
-"""
 class VersionMessage(Message):
-    command = b"version"
 
-    def __init__(
-            self,
-            version,
-            services,
-            time,
-            addr_recv,
-            addr_from,
-            nonce,
-            user_agent,
-            start_height,
-            relay,
-    ):
+    def __init__(self,
+                 version,
+                 services,
+                 addr_recv,
+                 addr_from,
+                 user_agent,
+                 start_height,
+                 relay):
         self.version = version
         self.services = services
-        self.time = time
+        self.timestamp = int(time.time())
         self.addr_recv = addr_recv
         self.addr_from = addr_from
-        self.nonce = nonce
+        self.nonce = random.getrandbits(64)
         self.user_agent = user_agent
         self.start_height = start_height
         self.relay = relay
-"""
+        self.payload = b""
+        self.command = b"version"
+        Message.__init__(self, command=self.command, payload=self.payload)
+
+    def generate_nonce(self):
+        self.nonce = random.getrandbits(64)
+        return self.nonce
+
+    def get_current_start_height(self):
+        # TODO: implement this
+        pass
+
+    def to_bytes(self):
+        msg = b""
+        msg += struct.pack('<I', self.version)
+        msg += struct.pack('<Q', self.services)
+        msg += struct.pack("<I", int(time.time()))
+        msg += self.addr_recv.addr_NT
+        msg += self.addr_from.addr_NT
+        msg += struct.pack('<Q', self.generate_nonce())
+        msg += super().to_var_str(self.user_agent)
+        msg += struct.pack('<I', self.start_height)
+        msg += struct.pack('?', self.relay)
+        self.payload = msg
+        return Message.to_bytes(self)
+
+    assigned_services = {hex(1): 'NODE_NETWORK',
+                         hex(2): 'NODE_GETUTXO',
+                         hex(4): 'NODE_BLOOM',
+                         hex(8): 'NODE_WITNESS',
+                         hex(1024): 'NODE_NETWORK_LIMITED'
+                         }
+
+
+class Verack(Message):
+
+    def __init__(self):
+        self.command = b"verack"
+        self.payload = b""
+        Message.__init__(self, command=self.command, payload=self.payload)
+
+
+class Addr(Message):
+
+    def __init__(self):
+        self.command = b"addr"
+        self.payload = b""
+        Message.__init__(self, command=self.command, payload=self.payload)
